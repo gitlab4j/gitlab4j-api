c2af9a23627fee74749548a1656a510291d477c2
Greg Messner <greg@cyberflowanalytics.com>
2017-02-06 00:22:47 -0800

    More mods to support GitLab 8

---
+++ src/main/java/com/messners/gitlab/api/webhook/EventCommit.java
@@ -0,0 +1,82 @@
+package com.messners.gitlab.api.webhook;
+
+import java.util.Date;
+import java.util.List;
+
+import com.messners.gitlab.api.models.Author;
+
+public class EventCommit {
+
+    private String id;
+    private String message;
+    private Date timestamp;
+    private String url;
+    private Author author;
+    private List<String> added;
+    private List<String> modified;
+    private List<String> removed;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<String> getAdded() {
+        return added;
+    }
+
+    public void setAdded(List<String> added) {
+        this.added = added;
+    }
+
+    public List<String> getModified() {
+        return modified;
+    }
+
+    public void setModified(List<String> modified) {
+        this.modified = modified;
+    }
+
+    public List<String> getRemoved() {
+        return removed;
+    }
+
+    public void setRemoved(List<String> removed) {
+        this.removed = removed;
+    }
+}
---
+++ src/main/java/com/messners/gitlab/api/webhook/EventProject.java
@@ -0,0 +1,138 @@
+package com.messners.gitlab.api.webhook;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+import com.messners.gitlab.api.models.AccessLevel;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class EventProject {
+
+    private String name;
+    private String description;
+    private String webUrl;
+    private String avatarUrl;
+    private String gitSshUrl;
+    private String gitHttpUrl;
+    private String namespace;
+    private AccessLevel visibilityLevel;
+    private String pathWithNamespace;
+    private String defaultBranch;
+    private String homepage;
+    private String url;
+    private String sshUrl;
+    private String httpUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getGitSshUrl() {
+        return gitSshUrl;
+    }
+
+    public void setGitSshUrl(String gitSshUrl) {
+        this.gitSshUrl = gitSshUrl;
+    }
+
+    public String getGitHttpUrl() {
+        return gitHttpUrl;
+    }
+
+    public void setGitHttpUrl(String gitHttpUrl) {
+        this.gitHttpUrl = gitHttpUrl;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public AccessLevel getVisibilityLevel() {
+        return visibilityLevel;
+    }
+
+    public void setVisibilityLevel(AccessLevel visibilityLevel) {
+        this.visibilityLevel = visibilityLevel;
+    }
+
+    public String getPathWithNamespace() {
+        return pathWithNamespace;
+    }
+
+    public void setPathWithNamespace(String pathWithNamespace) {
+        this.pathWithNamespace = pathWithNamespace;
+    }
+
+    public String getDefaultBranch() {
+        return defaultBranch;
+    }
+
+    public void setDefaultBranch(String defaultBranch) {
+        this.defaultBranch = defaultBranch;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSshUrl() {
+        return sshUrl;
+    }
+
+    public void setSshUrl(String sshUrl) {
+        this.sshUrl = sshUrl;
+    }
+
+    public String getHttpUrl() {
+        return httpUrl;
+    }
+
+    public void setHttpUrl(String httpUrl) {
+        this.httpUrl = httpUrl;
+    }
+
+}
