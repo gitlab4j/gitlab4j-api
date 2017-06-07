0d5cb6b671359f4b26d8cd8269b30662b4975158
Khosrow Moossavi <khosrow.moossavi@nakisa.com>
2017-06-07 10:42:57 -0400

    Add Pipeline API

---
+++ src/main/java/org/gitlab4j/api/models/Pipeline.java
@@ -0,0 +1,139 @@
+package org.gitlab4j.api.models;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Pipeline {
+
+    private Integer id;
+    private String status;
+    private String ref;
+    private String sha;
+    private String beforeSha;
+    private String yamlErrors;
+    private User user;
+    private Date createdAt;
+    private Date updated_at;
+    private Date started_at;
+    private Date finished_at;
+    private Date committed_at;
+    private String coverage;
+    private Integer duration;
+    
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getRef() {
+		return ref;
+	}
+
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+
+	public String getSha() {
+		return sha;
+	}
+
+	public void setSha(String sha) {
+		this.sha = sha;
+	}
+
+	public String getBeforeSha() {
+		return beforeSha;
+	}
+
+	public void setBeforeSha(String beforeSha) {
+		this.beforeSha = beforeSha;
+	}
+
+	public String getYamlErrors() {
+		return yamlErrors;
+	}
+
+	public void setYamlErrors(String yamlErrors) {
+		this.yamlErrors = yamlErrors;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(Date updated_at) {
+		this.updated_at = updated_at;
+	}
+
+	public Date getStarted_at() {
+		return started_at;
+	}
+
+	public void setStarted_at(Date started_at) {
+		this.started_at = started_at;
+	}
+
+	public Date getFinished_at() {
+		return finished_at;
+	}
+
+	public void setFinished_at(Date finished_at) {
+		this.finished_at = finished_at;
+	}
+
+	public Date getCommitted_at() {
+		return committed_at;
+	}
+
+	public void setCommitted_at(Date committed_at) {
+		this.committed_at = committed_at;
+	}
+
+	public String getCoverage() {
+		return coverage;
+	}
+
+	public void setCoverage(String coverage) {
+		this.coverage = coverage;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+}
