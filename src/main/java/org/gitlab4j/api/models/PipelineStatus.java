73d38d0092e2f99878283ca06ac71e37775fe908
Greg Messner <greg@messners.com>
2017-06-10 14:38:16 -0700

    Original check-in.

---
+++ src/main/java/org/gitlab4j/api/models/PipelineStatus.java
@@ -0,0 +1,36 @@
+package org.gitlab4j.api.models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * Enum for the various Pipeline status values.
+ */
+public enum PipelineStatus {
+
+    RUNNING, PENDING, SUCCESS, FAILED, CANCELED, SKIPPED;
+
+    private static Map<String, PipelineStatus> valuesMap = new HashMap<>(6);
+    static {
+        for (PipelineStatus status : PipelineStatus.values())
+            valuesMap.put(status.toValue(), status);
+    }
+
+    @JsonCreator
+    public static PipelineStatus forValue(String value) {
+        return valuesMap.get(value);
+    }
+
+    @JsonValue
+    public String toValue() {
+        return (name().toLowerCase());
+    }
+
+    @Override
+    public String toString() {
+        return (name().toLowerCase());
+    }
+}
