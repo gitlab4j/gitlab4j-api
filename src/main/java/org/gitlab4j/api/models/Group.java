03fe369164feb523bebd961e493053be5638e320
Olivér Spingár <spingar.oliver@gmail.com>
2017-06-13 14:02:57 +0200

    fix Group model and complete missing elements

--- src/main/java/org/gitlab4j/api/models/Group.java
+++ src/main/java/org/gitlab4j/api/models/Group.java
@@ -1,11 +1,10 @@
 
 package org.gitlab4j.api.models;
 
-import java.util.List;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
@@ -13,9 +12,15 @@ public class Group {
 
     private Integer id;
     private String name;
-    private Integer ownerId;
+    private Integer parentId;
     private String path;
     private List<Project> projects;
+    private String description;
+    private String visibility;
+    private String fullName;
+    private String fullPath;
+    private Boolean requestAccessEnabled;
+    private Boolean lfsEnabled;
 
     public Integer getId() {
         return this.id;
@@ -33,12 +38,12 @@ public class Group {
         this.name = name;
     }
 
-    public Integer getOwnerId() {
-        return this.ownerId;
+    public Integer getParentId() {
+        return parentId;
     }
 
-    public void setOwnerId(Integer ownerId) {
-        this.ownerId = ownerId;
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
     }
 
     public String getPath() {
@@ -55,5 +60,53 @@ public class Group {
 
     public void setProjects(List<Project> projects) {
         this.projects = projects;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getFullPath() {
+        return fullPath;
+    }
+
+    public void setFullPath(String fullPath) {
+        this.fullPath = fullPath;
+    }
+
+    public Boolean getRequestAccessEnabled() {
+        return requestAccessEnabled;
+    }
+
+    public void setRequestAccessEnabled(Boolean requestAccessEnabled) {
+        this.requestAccessEnabled = requestAccessEnabled;
+    }
+
+    public Boolean getLfsEnabled() {
+        return lfsEnabled;
+    }
+
+    public void setLfsEnabled(Boolean lfsEnabled) {
+        this.lfsEnabled = lfsEnabled;
     }
 }
