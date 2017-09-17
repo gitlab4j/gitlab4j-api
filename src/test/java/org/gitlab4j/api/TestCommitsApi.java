ca72221b94bf8e6e40af4adc73e007dffa634fdb
Greg Messner <greg@messners.com>
2017-09-17 11:59:48 -0700

    Removed unused import.

--- src/test/java/org/gitlab4j/api/TestCommitsApi.java
+++ src/test/java/org/gitlab4j/api/TestCommitsApi.java
@@ -2,7 +2,6 @@ package org.gitlab4j.api;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
