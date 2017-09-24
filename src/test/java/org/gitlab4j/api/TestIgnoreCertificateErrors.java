964c62a6a762df560c67a0cb6a3337b7ee44152a
Greg Messner <greg@messners.com>
2017-09-24 14:37:55 -0700

    Initial check-in (#80)

---
+++ src/test/java/org/gitlab4j/api/TestIgnoreCertificateErrors.java
@@ -0,0 +1,108 @@
+package org.gitlab4j.api;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
+
+import org.gitlab4j.api.GitLabApi.ApiVersion;
+import org.gitlab4j.api.models.Version;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * In order for these tests to run you must set the following properties in test-gitlab4j.properties
+ * 
+ * TEST_HOST_URL
+ * TEST_PRIVATE_TOKEN
+ * 
+ * If any of the above are NULL, all tests in this class will be skipped.
+ *
+ */
+public class TestIgnoreCertificateErrors {
+
+    // The following needs to be set to your test repository
+    private static final String TEST_HOST_URL;
+    private static final String TEST_PRIVATE_TOKEN;
+    static {
+        TEST_HOST_URL = TestUtils.getProperty("TEST_HOST_URL");
+        TEST_PRIVATE_TOKEN = TestUtils.getProperty("TEST_PRIVATE_TOKEN");
+    }
+
+    private static boolean setupOk;
+
+    public TestIgnoreCertificateErrors() {
+        super();
+    }
+
+    @BeforeClass
+    public static void setup() {
+
+        String problems = "";
+        if (TEST_HOST_URL == null || TEST_HOST_URL.trim().length() == 0) {
+            problems += "TEST_HOST_URL cannot be empty\n";
+        }
+
+        if (TEST_PRIVATE_TOKEN == null || TEST_PRIVATE_TOKEN.trim().length() == 0) {
+            problems += "TEST_PRIVATE_TOKEN cannot be empty\n";
+        }
+
+        if (problems.isEmpty()) {
+            setupOk = true;
+        } else {
+            setupOk = false;
+            System.err.print(problems);
+        }
+    }
+
+    @Before
+    public void beforeMethod() {
+        assumeTrue(setupOk);
+    }
+
+    @Test
+    public void testGetVersion() throws GitLabApiException {
+
+        GitLabApi gitLabApi = new GitLabApi(ApiVersion.V4, TEST_HOST_URL, TEST_PRIVATE_TOKEN);
+        assertFalse(gitLabApi.getIgnoreCertificateErrors());
+        Version version = gitLabApi.getVersion();
+        assertNotNull(version);
+        System.out.format("ignoreCertErrors: %b, version=%s, revision=%s%n", gitLabApi.getIgnoreCertificateErrors(), version.getVersion(), version.getRevision());
+        assertNotNull(version.getVersion());
+        assertNotNull(version.getRevision());
+
+        gitLabApi.setIgnoreCertificateErrors(true);
+        assertTrue(gitLabApi.getIgnoreCertificateErrors());
+        version = gitLabApi.getVersion();
+        assertNotNull(version);
+        System.out.format("ignoreCertErrors:  %b, version=%s, revision=%s%n", gitLabApi.getIgnoreCertificateErrors(), version.getVersion(), version.getRevision());
+        assertNotNull(version.getVersion());
+        assertNotNull(version.getRevision());
+
+        gitLabApi.setIgnoreCertificateErrors(false);
+        assertFalse(gitLabApi.getIgnoreCertificateErrors());
+        version = gitLabApi.getVersion();
+        assertNotNull(version);
+        System.out.format("ignoreCertErrors: %b, version=%s, revision=%s%n", gitLabApi.getIgnoreCertificateErrors(), version.getVersion(), version.getRevision());
+        assertNotNull(version.getVersion());
+        assertNotNull(version.getRevision());
+
+        gitLabApi = new GitLabApi(ApiVersion.V4, TEST_HOST_URL, TEST_PRIVATE_TOKEN);
+        gitLabApi.setIgnoreCertificateErrors(true);
+        assertTrue(gitLabApi.getIgnoreCertificateErrors());
+        version = gitLabApi.getVersion();
+        assertNotNull(version);
+        System.out.format("ignoreCertErrors:  %b, version=%s, revision=%s%n", gitLabApi.getIgnoreCertificateErrors(), version.getVersion(), version.getRevision());
+        assertNotNull(version.getVersion());
+        assertNotNull(version.getRevision());
+
+        gitLabApi.setIgnoreCertificateErrors(false);
+        assertFalse(gitLabApi.getIgnoreCertificateErrors());
+        version = gitLabApi.getVersion();
+        assertNotNull(version);
+        System.out.format("ignoreCertErrors: %b, version=%s, revision=%s%n", gitLabApi.getIgnoreCertificateErrors(), version.getVersion(), version.getRevision());
+        assertNotNull(version.getVersion());
+        assertNotNull(version.getRevision());
+    }
+}
