0b2d59dd096071ca1c158e4990d813bdad6ba818
Greg Messner <greg@messners.com>
2017-10-23 11:02:03 -0700

    Added tests for w (weeks) in duration.

--- src/test/java/org/gitlab4j/api/TestDuration.java
+++ src/test/java/org/gitlab4j/api/TestDuration.java
@@ -10,10 +10,13 @@ public class TestDuration {
 
     @Test
     public void testParse() {
-        
-        int seconds = DurationUtils.parse("1d1h1m1s");
+
+        int seconds = DurationUtils.parse("1w1d1h1m1s");
+        assertEquals(60 * 60 * 24 * 7 + 60 * 60 * 24 + 60 * 60 + 60 + 1, seconds);
+
+        seconds = DurationUtils.parse("1d1h1m1s");
         assertEquals(60 * 60 * 24 + 60 * 60 + 60 + 1, seconds);
-        
+
         seconds = DurationUtils.parse("60m");
         assertEquals(60 * 60, seconds);
  
@@ -51,18 +54,28 @@ public class TestDuration {
         } catch (IllegalArgumentException iae) {
             System.out.println("Recieved expected exception: " + iae.getMessage());
         }
+
+        try {
+            DurationUtils.parse("1w2w2d2h2m");
+            fail("Should have received an exception for the bad duration");
+        } catch (IllegalArgumentException iae) {
+            System.out.println("Recieved expected exception: " + iae.getMessage());
+        }
     }
     
     @Test
     public void testToString() {
-        
+
         String duration = DurationUtils.toString(60 + 1);
         assertEquals("1m1s", duration);
-        
+
         duration = DurationUtils.toString(60 * 60 + 60 + 1);
         assertEquals("1h1m1s", duration);
-        
+
         duration = DurationUtils.toString(60 * 60 * 24 + 60 * 60 * 2 + 60 * 3 + 4);
         assertEquals("1d2h3m4s", duration);
+
+        duration = DurationUtils.toString(60 * 60 * 24 * 7 + 60 * 60 * 24 * 2 + 60 * 60 * 3 + 60 * 4 + 5);
+        assertEquals("1w2d3h4m5s", duration);
     }
 }
