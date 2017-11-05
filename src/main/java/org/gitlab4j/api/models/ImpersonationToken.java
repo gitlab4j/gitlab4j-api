24d2f1feac038023495c4ff5a1faca0ee648d0fe
Greg Messner <greg@messners.com>
2017-11-05 06:22:24 -0800

    Initial check-in (#91).

---
+++ src/main/java/org/gitlab4j/api/models/ImpersonationToken.java
@@ -0,0 +1,121 @@
+package org.gitlab4j.api.models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+import org.gitlab4j.api.utils.JacksonJsonEnumHelper;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ImpersonationToken {
+
+    /** Enum to specify the scope of an ImpersonationToken. */
+    public enum Scope {
+
+        API, READ_USER;
+
+        private static JacksonJsonEnumHelper<Scope> enumHelper = new JacksonJsonEnumHelper<>(Scope.class);
+
+        @JsonCreator
+        public static Scope forValue(String value) {
+            return enumHelper.forValue(value);
+        }
+
+        @JsonValue
+        public String toValue() {
+            return (enumHelper.toString(this));
+        }
+
+        @Override
+        public String toString() {
+            return (enumHelper.toString(this));
+        }
+    }
+
+    private Boolean active;
+    private String token;
+    private List<Scope> scopes;
+    private Boolean revoked;
+    private String name;
+    private Integer id;
+    private Date createdAt;
+    private Boolean impersonation;
+    private Date expiresAt;
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public List<Scope> getScopes() {
+        return scopes;
+    }
+
+    public void setScopes(List<Scope> scopes) {
+        this.scopes = scopes;
+    }
+
+    public Boolean getRevoked() {
+        return revoked;
+    }
+
+    public void setRevoked(Boolean revoked) {
+        this.revoked = revoked;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Boolean getImpersonation() {
+        return impersonation;
+    }
+
+    public void setImpersonation(Boolean impersonation) {
+        this.impersonation = impersonation;
+    }
+
+    public Date getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Date expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+}
