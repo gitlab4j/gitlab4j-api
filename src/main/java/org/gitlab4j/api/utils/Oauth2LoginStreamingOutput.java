53f8f8efd1c3c58a2552f1e9b66376088dab5f4a
Greg Messner <greg@messners.com>
2018-03-12 15:43:40 -0700

    Initial commit (#155).

---
+++ src/main/java/org/gitlab4j/api/utils/Oauth2LoginStreamingOutput.java
@@ -0,0 +1,69 @@
+package org.gitlab4j.api.utils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.StreamingOutput;
+
+/**
+ * This StreamingOutput implementation is utilized to send a OAuth2 token request
+ * in a secure manner.  The password is never copied to a String, instead it is
+ * contained in a SecretString that is cleared when an instance of this class is finalized.
+ */
+public class Oauth2LoginStreamingOutput implements StreamingOutput, AutoCloseable {
+
+    private final String username;
+    private final SecretString password;
+
+    public Oauth2LoginStreamingOutput(String username, CharSequence password) {
+        this.username = username;
+        this.password = new SecretString(password);
+    }
+
+    public Oauth2LoginStreamingOutput(String username, char[] password) {
+        this.username = username;
+        this.password = new SecretString(password);
+    }
+
+    @Override
+    public void write(OutputStream output) throws IOException, WebApplicationException {
+
+        PrintWriter writer = new PrintWriter(output);
+        writer.append("{ ");
+        writer.append("\"grant_type\": \"password\", ");
+        writer.append("\"username\": \"" + username + "\", ");
+        writer.append("\"password\": ");
+
+        // Output the quoted password
+        writer.append('"');
+        for (int i = 0, length = password.length(); i < length; i++) {
+            char c = password.charAt(i);
+            writer.append(c);
+        }     
+        writer.append('"');
+
+        writer.append(" }");
+        writer.flush();
+        writer.close();
+    }
+
+    /**
+     * Clears the contained password's data.
+     */
+    public void clearPassword() {
+        password.clear();
+    }
+
+    @Override
+    public void close() {
+        password.clear();
+    }
+
+    @Override
+    public void finalize() throws Throwable {
+        clearPassword();
+        super.finalize();
+    }
+}
