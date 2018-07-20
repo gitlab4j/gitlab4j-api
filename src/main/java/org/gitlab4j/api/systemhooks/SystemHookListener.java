092bcaf75d5cfdd526900c78483b670f97d60b73
Greg Messner <greg@messners.com>
2018-07-20 11:12:21 -0700

    Defined all methods as default to simplify implementations.

--- src/main/java/org/gitlab4j/api/systemhooks/SystemHookListener.java
+++ src/main/java/org/gitlab4j/api/systemhooks/SystemHookListener.java
@@ -2,7 +2,7 @@
 package org.gitlab4j.api.systemhooks;
 
 /**
- * This class defines an event listener for the event fired when
+ * This interface defines an event listener for the event fired when
  * a System Hook notification has been received from a GitLab server.
  */
 public interface SystemHookListener extends java.util.EventListener {
@@ -12,61 +12,70 @@ public interface SystemHookListener extends java.util.EventListener {
      *
      * @param event the ProjectSystemHookEvent instance
      */
-    public void onProjectEvent(ProjectSystemHookEvent event);
+    default void onProjectEvent(ProjectSystemHookEvent event) {
+    }
 
     /**
      * This method is called when a System Hook team member event has been received.
      *
      * @param event the TeamMemberSystemHookEvent instance containing info on the team member event
      */
-    public void onTeamMemberEvent(TeamMemberSystemHookEvent event);
+    default void onTeamMemberEvent(TeamMemberSystemHookEvent event) {
+    }
 
     /**
      * This method is called when a System Hook user event has been received.
      *
      * @param event the UserSystemHookEvent instance containing info on the user event
      */
-    public void onUserEvent(UserSystemHookEvent event);
+    default void onUserEvent(UserSystemHookEvent event) {
+    }
 
     /**
      * This method is called when a System Hook key event has been received.
      *
      * @param event the KeySystemHookEvent instance containing info on the key event
      */
-    public void onKeyEvent(KeySystemHookEvent event);
+    default void onKeyEvent(KeySystemHookEvent event) {
+    }
 
     /**
      * This method is called when a System Hook group event has been received.
      *
      * @param event the GroupSystemHookEvent instance containing info on the key event
      */
-    public void onGroupEvent(GroupSystemHookEvent event);
+    default void onGroupEvent(GroupSystemHookEvent event) {
+    }
 
     /**
      * This method is called when a System Hook group member event has been received.
      *
      * @param event the GroupMemberSystemHookEvent instance containing info on the key event
      */
-    public void onGroupMemberEvent(GroupMemberSystemHookEvent event);
+    default void onGroupMemberEvent(GroupMemberSystemHookEvent event) {
+    }
 
     /**
      * This method is called when a System Hook push event has been received.
      *
      * @param event the PushSystemHookEvent instance containing info on the key event
      */
-    public void onPushEvent(PushSystemHookEvent event);
+    default void onPushEvent(PushSystemHookEvent event) {
+    }
 
     /**
      * This method is called when a System Hook tag push event has been received.
      *
      * @param event the TagPushSystemHookEvent instance containing info on the key event
      */
-    public void onTagPushEvent(TagPushSystemHookEvent event);
+    default void onTagPushEvent(TagPushSystemHookEvent event) {
+    }
 
     /**
      * This method is called when a System Hook repository event has been received.
      *
      * @param event the RepositorySystemHookEvent instance containing info on the key event
      */
-    public void onRepositoryEvent(RepositorySystemHookEvent event);
+    default void onRepositoryEvent(RepositorySystemHookEvent event) {
+    }
 }
