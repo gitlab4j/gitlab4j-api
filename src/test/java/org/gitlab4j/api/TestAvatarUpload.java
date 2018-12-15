51048544ed18b5c558be28af98c6e3ff583ceac4
Greg Messner <greg@messners.com>
2018-12-15 13:32:00 -0800

    Removed unused import.

--- src/test/java/org/gitlab4j/api/TestAvatarUpload.java
+++ src/test/java/org/gitlab4j/api/TestAvatarUpload.java
@@ -1,7 +1,6 @@
 package org.gitlab4j.api;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
