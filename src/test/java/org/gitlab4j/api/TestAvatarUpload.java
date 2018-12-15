a39ae6d053d0a4339883b5d9b64001f7ae30eba5
Greg Messner <greg@messners.com>
2018-12-15 11:24:25 -0800

    Initial commit to support setting project avatar (#284).

---
+++ src/test/java/org/gitlab4j/api/TestAvatarUpload.java
@@ -0,0 +1,104 @@
+package org.gitlab4j.api;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeTrue;
+
+import java.io.File;
+
+import org.gitlab4j.api.GitLabApi.ApiVersion;
+import org.gitlab4j.api.models.Project;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+/**
+* In order for these tests to run you must set the following properties in test-gitlab4j.properties
+ * 
+ * TEST_NAMESPACE
+ * TEST_PROJECT_NAME
+ * TEST_HOST_URL
+ * TEST_PRIVATE_TOKEN
+ * 
+ * If any of the above are NULL, all tests in this class will be skipped.
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestAvatarUpload {
+
+    // The following needs to be set to your test repository
+    private static final String TEST_PROJECT_NAME;
+    private static final String TEST_NAMESPACE;
+    private static final String TEST_HOST_URL;
+    private static final String TEST_PRIVATE_TOKEN;
+    static {
+        TEST_NAMESPACE = TestUtils.getProperty("TEST_NAMESPACE");
+        TEST_PROJECT_NAME = TestUtils.getProperty("TEST_PROJECT_NAME");
+        TEST_HOST_URL = TestUtils.getProperty("TEST_HOST_URL");
+        TEST_PRIVATE_TOKEN = TestUtils.getProperty("TEST_PRIVATE_TOKEN");
+    }
+
+    private static GitLabApi gitLabApi;
+
+    public TestAvatarUpload() {
+        super();
+    }
+
+    @BeforeClass
+    public static void setup() {
+
+        String problems = "";
+        if (TEST_NAMESPACE == null || TEST_NAMESPACE.trim().isEmpty()) {
+            problems += "TEST_NAMESPACE cannot be empty\n";
+        }
+
+        if (TEST_PROJECT_NAME == null || TEST_PROJECT_NAME.trim().isEmpty()) {
+            problems += "TEST_PROJECT_NAME cannot be empty\n";
+        }
+
+        if (TEST_HOST_URL == null || TEST_HOST_URL.trim().isEmpty()) {
+            problems += "TEST_HOST_URL cannot be empty\n";
+        }
+
+        if (TEST_PRIVATE_TOKEN == null || TEST_PRIVATE_TOKEN.trim().isEmpty()) {
+            problems += "TEST_PRIVATE_TOKEN cannot be empty\n";
+        }
+
+        if (problems.isEmpty()) {   
+            gitLabApi = new GitLabApi(ApiVersion.V4, TEST_HOST_URL, TEST_PRIVATE_TOKEN);
+        } else {
+            System.err.print(problems);
+        }
+    }
+
+    @Before
+    public void beforeMethod() {
+        assumeTrue(gitLabApi != null);
+    }
+
+    @Test
+    public void testSetProjectAvatar() throws GitLabApiException {
+
+        Project project = gitLabApi.getProjectApi().getProject(TEST_NAMESPACE, TEST_PROJECT_NAME);
+        assertNotNull(project);
+
+        File avatarFile = new File("src/test/resources/org/gitlab4j/api/avatar.png");
+        Project updatedProject = gitLabApi.getProjectApi().setProjectAvatar(project.getId(), avatarFile, null);
+        assertNotNull(updatedProject);
+        assertTrue(updatedProject.getAvatarUrl().endsWith("avatar.png"));
+    }
+
+    @Test
+    public void testSetProjectAvatarWithMediaType() throws GitLabApiException {
+
+        Project project = gitLabApi.getProjectApi().getProject(TEST_NAMESPACE, TEST_PROJECT_NAME);
+        assertNotNull(project);
+
+        File avatarFile = new File("src/test/resources/org/gitlab4j/api/avatar.png");
+        Project updatedProject = gitLabApi.getProjectApi().setProjectAvatar(project.getId(), avatarFile, "image/png");
+        assertNotNull(updatedProject);
+        assertTrue(updatedProject.getAvatarUrl().endsWith("avatar.png"));
+    }
+}
