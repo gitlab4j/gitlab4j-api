acb7de3eb6afeda1f7df90f197a62016b390bba9
Greg Messner <greg@messners.com>
2018-12-16 18:05:39 -0800

    Added methods to return the JSON for both the item and list.

--- src/test/java/org/gitlab4j/api/FakeResponse.java
+++ src/test/java/org/gitlab4j/api/FakeResponse.java
@@ -1,47 +1,58 @@
 package org.gitlab4j.api;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 
 /**
- * This class can be used as a spy for Mockito to test the individual APIs getXxxxx() methods without the
- * need to have a GitLab server available.
+ * This class can be used as a spy for Mockito to test the individual APIs
+ * getXxxxx() methods without the need to have a GitLab server available.
  * 
- * Supports getXxxxx() methods that return a List of items, single items, and Optional items.
+ * Supports getXxxxx() methods that return a List of items, single items,
+ * Optional items, and Pagers of items.
  */
 public abstract class FakeResponse extends Response {
 
     private List<?> responseList;
-    private ByteArrayInputStream responseInputStream;
     private Object responseItem;
     private int perPage = 20;
+
+    private String itemJson;
+    private String listJson;
 
     public <T> void init(Class<T> type, String itemFilename, String listFilename) throws Exception {
 
         if (itemFilename != null) {
-            InputStreamReader reader = new InputStreamReader(TestGitLabApiBeans.class.getResourceAsStream(itemFilename));
-            responseItem = JsonUtils.unmarshal(type, reader);
+            itemJson = JsonUtils.readResource(itemFilename);
+            responseItem = JsonUtils.unmarshal(type, itemJson);
         } else {
             responseItem = null;
         }
 
         if (listFilename != null) {
-            String listJson = JsonUtils.readResource(listFilename);
+            listJson = JsonUtils.readResource(listFilename);
             responseList = (List<?>) JsonUtils.unmarshalList(type, listJson);
-            responseInputStream = new ByteArrayInputStream(listJson.getBytes());
         } else {
             responseList = null;
-            responseInputStream = null;
         }
+    }
+
+    public String getItemJson() {
+        return (itemJson);
+    }
+
+    public String getListJson() {
+        return (listJson);
     }
 
     public void setPerPageHeaderValue(int perPage) {
         this.perPage = perPage;
     }
+
+    // The below methods allow this abstract class to act as a fake Resource
+    // instance.
 
     @SuppressWarnings("unchecked")
     @Override
@@ -57,12 +68,7 @@ public abstract class FakeResponse extends Response {
 
     @Override
     public Object getEntity() {
-        if (responseInputStream == null) {
-            return (null);
-        }
-
-        responseInputStream.reset();
-        return (responseInputStream);
+        return (listJson != null ? new ByteArrayInputStream(listJson.getBytes()) : null);
     }
 
     @Override
@@ -72,7 +78,8 @@ public abstract class FakeResponse extends Response {
             return (responseList != null ? Integer.toString(perPage) : "0");
 
         case Constants.TOTAL_PAGES_HEADER:
-            return (responseList != null ? Integer.toString((int)Math.ceil((double)responseList.size() / 20.0)) : "0");
+            return (responseList != null ? Integer.toString((int) Math.ceil((double) responseList.size() / 20.0))
+                    : "0");
 
         case Constants.TOTAL_HEADER:
             return (responseList != null ? Integer.toString(responseList.size()) : "0");
