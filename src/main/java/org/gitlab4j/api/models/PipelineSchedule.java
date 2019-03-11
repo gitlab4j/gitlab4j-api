ee9c6a2ef8443f0fa2de65e2b7e99f223d76ac7d
lpiet <louis.piet@gmail.com>
2019-03-11 17:46:34 +0100

    pipeline schedule support

---
+++ src/main/java/org/gitlab4j/api/models/PipelineSchedule.java
@@ -0,0 +1,119 @@
+ckage org.gitlab4j.api.models;
+
+import org.gitlab4j.api.utils.JacksonJson;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PipelineSchedule {
+
+    private Integer id;
+    private String description;
+    private String ref;
+    private String cron;
+    private String cronTimezone;
+    private Date nextRunAt;
+    private Date createdAt;
+    private Date updatedAt;
+    private Boolean active;
+    private Pipeline lastPipeline;
+    private Owner owner;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRef() {
+        return ref;
+    }
+
+    public void setRef(String ref) {
+        this.ref = ref;
+    }
+
+    public String getCron() {
+        return cron;
+    }
+
+    public void setCron(String cron) {
+        this.cron = cron;
+    }
+
+    public String getCronTimezone() {
+        return cronTimezone;
+    }
+
+    public void setCronTimezone(String cronTimezone) {
+        this.cronTimezone = cronTimezone;
+    }
+
+    public Date getNextRunAt() {
+        return nextRunAt;
+    }
+
+    public void setNextRunAt(Date nextRunAt) {
+        this.nextRunAt = nextRunAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Pipeline getLastPipeline() {
+        return lastPipeline;
+    }
+
+    public void setLastPipeline(Pipeline lastPipeline) {
+        this.lastPipeline = lastPipeline;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return (JacksonJson.toJsonString(this));
+    }
+}
+
