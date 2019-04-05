e82dfb0d40a46276e9357d109952fc4e6d3df7bf
Greg Messner <greg@messners.com>
2019-04-05 14:59:59 -0700

    Initial commit (#326).

---
+++ src/main/java/org/gitlab4j/api/models/Variable.java
@@ -0,0 +1,29 @@
+package org.gitlab4j.api.models;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Variable {
+
+    private String key;
+    private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
