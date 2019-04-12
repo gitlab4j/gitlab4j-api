2ce86a2fe5f8435935315c0a298842703f03ce23
Greg Messner <greg@messners.com>
2019-04-11 19:23:25 -0700

    Fixed webhooks changes deserialization (#325, #331).

--- src/main/java/org/gitlab4j/api/webhook/EventChanges.java
+++ src/main/java/org/gitlab4j/api/webhook/EventChanges.java
@@ -2,42 +2,87 @@ package org.gitlab4j.api.webhook;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 
+import org.gitlab4j.api.models.Assignee;
 import org.gitlab4j.api.utils.JacksonJson;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class EventChanges {
 
-    private List<Integer> updatedById;
-    private List<Date> updatedAt;
-    private Map<String, List<EventLabel>> labels;
+    private ChangeContainer<Date> updatedAt;
+    private ChangeContainer<Integer> updatedById;
+    private ChangeContainer<Date> dueDate;
+    private ChangeContainer<Integer> milestoneId;
+    private ChangeContainer<List<EventLabel>> labels;
+    private ChangeContainer<List<Assignee>> assignees;
+    private ChangeContainer<Integer> totalTimeSpent;
+    private ChangeContainer<Boolean> confidential;
 
-    public List<Integer> getUpdatedById() {
-        return updatedById;
-    }
-
-    public void setUpdatedById(List<Integer> updatedById) {
-        this.updatedById = updatedById;
-    }
-
-    public List<Date> getUpdatedAt() {
+    public ChangeContainer<Date> getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(List<Date> updatedAt) {
+    public void setUpdatedAt(ChangeContainer<Date> updatedAt) {
         this.updatedAt = updatedAt;
     }
 
-    public Map<String, List<EventLabel>> getLabels() {
+    public ChangeContainer<Integer> getUpdatedById() {
+        return updatedById;
+    }
+
+    public void setUpdatedById(ChangeContainer<Integer> updatedById) {
+        this.updatedById = updatedById;
+    }
+
+    public ChangeContainer<Date> getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(ChangeContainer<Date> dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public ChangeContainer<Integer> getMilestoneId() {
+        return milestoneId;
+    }
+
+    public void setMilestoneId(ChangeContainer<Integer> milestoneId) {
+        this.milestoneId = milestoneId;
+    }
+
+    public ChangeContainer<List<EventLabel>> getLabels() {
         return labels;
     }
 
-    public void setLabels(Map<String, List<EventLabel>> labels) {
+    public void setLabels(ChangeContainer<List<EventLabel>> labels) {
         this.labels = labels;
+    }
+
+    public ChangeContainer<List<Assignee>> getAssignees() {
+        return assignees;
+    }
+
+    public void setAssignees(ChangeContainer<List<Assignee>> assignees) {
+        this.assignees = assignees;
+    }
+
+    public ChangeContainer<Integer> getTotalTimeSpent() {
+        return totalTimeSpent;
+    }
+
+    public void setTotalTimeSpent(ChangeContainer<Integer> totalTimeSpent) {
+        this.totalTimeSpent = totalTimeSpent;
+    }
+
+    public ChangeContainer<Boolean> getConfidential() {
+        return confidential;
+    }
+
+    public void setConfidential(ChangeContainer<Boolean> confidential) {
+        this.confidential = confidential;
     }
 
     @Override
