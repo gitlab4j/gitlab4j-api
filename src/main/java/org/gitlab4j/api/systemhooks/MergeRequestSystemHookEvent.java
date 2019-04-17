74d939480966b818bf882b98686ee4956f7fece2
Greg Messner <greg@messners.com>
2019-04-17 15:47:20 -0700

    Added event_type property.

--- src/main/java/org/gitlab4j/api/systemhooks/MergeRequestSystemHookEvent.java
+++ src/main/java/org/gitlab4j/api/systemhooks/MergeRequestSystemHookEvent.java
@@ -7,6 +7,8 @@ public class MergeRequestSystemHookEvent extends MergeRequestEvent implements Sy
     public static final String X_GITLAB_EVENT = "System Hook";
     public static final String MERGE_REQUEST_EVENT = "merge_request";
 
+    private String eventType;
+
     @Override
     public String getObjectKind() {
         return (MERGE_REQUEST_EVENT);
@@ -15,5 +17,13 @@ public class MergeRequestSystemHookEvent extends MergeRequestEvent implements Sy
     @Override
     public String getEventName() {
         return (MERGE_REQUEST_EVENT);
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEvent_type(String eventType) {
+        this.eventType = eventType;
     }
 }
