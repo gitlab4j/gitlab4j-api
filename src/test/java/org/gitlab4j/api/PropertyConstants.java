ea90e5653eaa1c5b92e33a9ee6ec47cff96d1a16
Greg Messner <greg@messners.com>
2019-05-01 18:44:57 -0700

    Initial commit (#311).

---
+++ src/test/java/org/gitlab4j/api/PropertyConstants.java
@@ -0,0 +1,25 @@
+package org.gitlab4j.api;
+
+public interface PropertyConstants {
+
+    public static final String TEST_PROPERTIES_FILENAME = "test-gitlab4j.properties";
+
+    public static final String ACCESS_TOKEN_KEY = "TEST_ACCESS_TOKEN";
+    public static final String BLOCK_USERNAME_KEY = "TEST_BLOCK_USERNAM";
+    public static final String GROUP_KEY = "TEST_GROUP";
+    public static final String GROUP_MEMBER_USERNAME_KEY = "TEST_GROUP_MEMBER_USERNAME";
+    public static final String GROUP_PROJECT_KEY = "TEST_GROUP_PROJECT";
+    public static final String HEALTH_CHECK_TOKEN_KEY = "TEST_HEALTH_CHECK_TOKEN";
+    public static final String HOST_URL_KEY = "TEST_HOST_URL";
+    public static final String LOGIN_PASSWORD_KEY = "TEST_LOGIN_PASSWORD";
+    public static final String LOGIN_USERNAME_KEY = "TEST_LOGIN_USERNAME";
+    public static final String NAMESPACE_KEY =  "TEST_NAMESPACE";
+    public static final String PRIVATE_TOKEN_KEY =  "TEST_PRIVATE_TOKEN";
+    public static final String PROJECT_NAME_KEY = "TEST_PROJECT_NAME";
+    public static final String PROXY_PASSWORD_KEY = "TEST_PROXY_PASSWORD";
+    public static final String PROXY_URI_KEY = "TEST_PROXY_URI";
+    public static final String PROXY_USERNAME_KEY = "TEST_PROXY_USERNAME";
+    public static final String SUDO_AS_USERNAME_KEY = "TEST_SUDO_AS_USERNAME";
+    public static final String USERNAME_KEY = "TEST_USERNAME";
+    public static final String XFER_NAMESPACE_KEY = "TEST_XFER_NAMESPACE";
+}
