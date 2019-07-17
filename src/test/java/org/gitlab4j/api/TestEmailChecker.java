07022cef135d3d25126a2b7eb9637422ceaa896a
Greg Messner <greg@messners.com>
2019-07-16 23:57:42 -0700

    Initial commit (#402).

---
+++ src/test/java/org/gitlab4j/api/TestEmailChecker.java
@@ -0,0 +1,45 @@
+package org.gitlab4j.api;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.gitlab4j.api.utils.EmailChecker;
+import org.junit.Test;
+
+public class TestEmailChecker {
+
+    @Test
+    public void testValidEmail() {
+        assertTrue(EmailChecker.isValidEmail("john@foobar.com"));
+    }
+
+    @Test
+    public void testValidEmailWithPeriods() {
+        assertTrue(EmailChecker.isValidEmail("john.doe@foobar.com"));
+    }
+
+    @Test
+    public void testMissingTld() {
+        assertFalse(EmailChecker.isValidEmail("john@foobar"));
+    }
+
+    @Test
+    public void testMissingHostname() {
+        assertFalse(EmailChecker.isValidEmail("sam@.com"));
+    }
+
+    @Test
+    public void testNameOnly() {
+        assertFalse(EmailChecker.isValidEmail("john"));
+    }
+
+    @Test
+    public void testNullEmail() {
+        assertFalse(EmailChecker.isValidEmail(null));
+    }
+
+    @Test
+    public void testEmptyEmail() {
+        assertFalse(EmailChecker.isValidEmail(""));
+    }
+}
