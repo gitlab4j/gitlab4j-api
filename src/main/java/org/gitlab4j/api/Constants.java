41f7cf82cfdaf80611e6706ff9809afd52f4b85a
Greg Messner <greg@messners.com>
2019-10-28 20:08:43 -0700

    Fixed TargetType enum (#454).

--- src/main/java/org/gitlab4j/api/Constants.java
+++ src/main/java/org/gitlab4j/api/Constants.java
@@ -466,7 +466,7 @@ public interface Constants {
 
         ISSUE, MILESTONE, MERGE_REQUEST, NOTE, PROJECT, SNIPPET, USER;
 
-        private static JacksonJsonEnumHelper<TargetType> enumHelper = new JacksonJsonEnumHelper<>(TargetType.class, true);
+        private static JacksonJsonEnumHelper<TargetType> enumHelper = new JacksonJsonEnumHelper<>(TargetType.class, true, true);
 
         @JsonCreator
         public static TargetType forValue(String value) {
