e75bfb08ca0f0c49f40b6dd5de11daf0b4dd927f
Greg Messner <greg@messners.com>
2019-11-09 17:03:57 -0800

    Added support for new health check JSON format (#468).

--- src/main/java/org/gitlab4j/api/models/HealthCheckInfo.java
+++ src/main/java/org/gitlab4j/api/models/HealthCheckInfo.java
@@ -1,14 +1,38 @@
 package org.gitlab4j.api.models;
 
+import java.io.IOException;
+
 import org.gitlab4j.api.utils.JacksonJson;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 public class HealthCheckInfo {
+
+    @JsonDeserialize(using = HealthCheckItemDeserializer.class)
     private HealthCheckItem dbCheck;
+
+    @JsonDeserialize(using = HealthCheckItemDeserializer.class)
     private HealthCheckItem redisCheck;
+
+    @JsonDeserialize(using = HealthCheckItemDeserializer.class)
     private HealthCheckItem cacheCheck;
+
+    @JsonDeserialize(using = HealthCheckItemDeserializer.class)
     private HealthCheckItem queuesCheck;
+
+    @JsonDeserialize(using = HealthCheckItemDeserializer.class)
     private HealthCheckItem sharedStateCheck;
+
+    @JsonDeserialize(using = HealthCheckItemDeserializer.class)
     private HealthCheckItem fsShardsCheck;
+
+    @JsonDeserialize(using = HealthCheckItemDeserializer.class)
     private HealthCheckItem gitalyCheck;
 
     public HealthCheckItem getDbCheck() {
@@ -70,5 +94,30 @@ public class HealthCheckInfo {
     @Override
     public String toString() {
         return (JacksonJson.toJsonString(this));
+    }
+
+    /**
+     * This desrializer can deserialize on object containing a HealthCheckItem or an
+     * array containing a single HealthCheckItem.
+     */
+    private static class HealthCheckItemDeserializer extends JsonDeserializer<HealthCheckItem> {
+
+        private static final ObjectMapper mapper = new JacksonJson().getObjectMapper();
+
+	@Override
+	public HealthCheckItem deserialize(JsonParser jsonParser, DeserializationContext ctx)
+		throws IOException, JsonProcessingException {
+
+	    HealthCheckItem healthCheckItem = null;
+	    JsonNode tree = jsonParser.readValueAsTree();
+	    if (tree.isArray()) {
+		JsonNode node = tree.get(0);
+		healthCheckItem = mapper.treeToValue(node,  HealthCheckItem.class);
+	    } else if (tree.isObject()) {
+		healthCheckItem = mapper.treeToValue(tree,  HealthCheckItem.class);
+	    }
+
+	    return (healthCheckItem);
+	}
     }
 }
