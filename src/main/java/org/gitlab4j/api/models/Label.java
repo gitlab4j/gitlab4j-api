4415063286331378e922c1eb84d052ab5f56ad03
Greg Messner <greg@messners.com>
2019-11-19 11:27:03 -0800

    Mods to support group labeles API (#477).

--- src/main/java/org/gitlab4j/api/models/Label.java
+++ src/main/java/org/gitlab4j/api/models/Label.java
@@ -1,6 +1,9 @@
 package org.gitlab4j.api.models;
 
+import org.gitlab4j.api.GitLabApiForm;
 import org.gitlab4j.api.utils.JacksonJson;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Label {
 
@@ -30,6 +33,11 @@ public class Label {
         this.name = name;
     }
 
+    public Label withName(String name) {
+        this.name = name;
+        return (this);
+    }
+
     public String getColor() {
         return color;
     }
@@ -38,12 +46,22 @@ public class Label {
         this.color = color;
     }
 
+    public Label withColor(String color) {
+        this.color = color;
+        return (this);
+    }
+
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Label withDescription(String description) {
+        this.description = description;
+        return (this);
     }
 
     public Integer getOpenIssuesCount() {
@@ -86,8 +104,35 @@ public class Label {
         this.priority = priority;
     }
 
+    public Label withPriority(Integer priority) {
+        this.priority = priority;
+        return (this);
+    }
+
     @Override
     public String toString() {
         return (JacksonJson.toJsonString(this));
+    }
+
+    /**
+     * Get the form params specified by this instance.
+     *
+     * @param isCreate set to true if the params are for a create label call, false for an update
+     * @return a GitLabApiForm instance holding the form parameters for this LabelParams instance
+     */
+    @JsonIgnore
+    public GitLabApiForm getForm(boolean isCreate) {
+	GitLabApiForm form = new GitLabApiForm()
+            .withParam("description", description)
+            .withParam("color", color, isCreate)
+            .withParam("priority", priority);
+
+	if (isCreate) {
+	    form.withParam("name", name, true);
+	} else {
+	    form.withParam("new_name", name);
+	}
+
+	return (form);
     }
 }
