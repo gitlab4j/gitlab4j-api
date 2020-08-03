ef66aab9d3a2d5074c1d3892f0b9f1fb7868c1af
Ladislav Kaiser <ladislav.kaiser@creativedock.com>
2020-08-03 13:14:37 +0200

    added buildCoverageRegex and buildGitStrategy to Project

--- src/main/java/org/gitlab4j/api/models/Project.java
+++ src/main/java/org/gitlab4j/api/models/Project.java
@@ -37,6 +37,29 @@ public class Project {
         }
     }
 
+    // Enum for the build_git_strategy of the project instance.
+    public enum BuildGitStrategy {
+
+        FETCH, CLONE;
+
+        private static JacksonJsonEnumHelper<BuildGitStrategy> enumHelper = new JacksonJsonEnumHelper<>(BuildGitStrategy.class);
+
+        @JsonCreator
+        public static BuildGitStrategy forValue(String value) {
+            return enumHelper.forValue(value);
+        }
+
+        @JsonValue
+        public String toValue() {
+            return (enumHelper.toString(this));
+        }
+
+        @Override
+        public String toString() {
+            return (enumHelper.toString(this));
+        }
+    }
+
     private Integer approvalsBeforeMerge;
     private Boolean archived;
     private String avatarUrl;
