80abfcae16f48ca73e2dbde463dcc71b9c2f678d
Jérémie Bresson <jeremie.bresson@unblu.com>
2024-02-10 12:18:19 +0100

    Activate FAIL_ON_UNKNOWN_PROPERTIES for tests in JsonUtils (#1089)

--- src/main/java/org/gitlab4j/api/systemhooks/MergeRequestSystemHookEvent.java
+++ src/main/java/org/gitlab4j/api/systemhooks/MergeRequestSystemHookEvent.java
@@ -2,6 +2,8 @@ package org.gitlab4j.api.systemhooks;
 
 import org.gitlab4j.api.webhook.MergeRequestEvent;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class MergeRequestSystemHookEvent extends MergeRequestEvent implements SystemHookEvent {
     private static final long serialVersionUID = 1L;
 
@@ -9,6 +11,7 @@ public class MergeRequestSystemHookEvent extends MergeRequestEvent implements Sy
     public static final String MERGE_REQUEST_EVENT = "merge_request";
 
     private String eventType;
+    private String eventName;
 
     @Override
     public String getObjectKind() {
@@ -17,14 +20,28 @@ public class MergeRequestSystemHookEvent extends MergeRequestEvent implements Sy
 
     @Override
     public String getEventName() {
-        return (MERGE_REQUEST_EVENT);
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
     }
 
     public String getEventType() {
         return eventType;
     }
-
-    public void setEvent_type(String eventType) {
+    
+    public void setEventType(String eventType) {
         this.eventType = eventType;
+    }
+
+    /**
+     * @deprecated use {@link #setEventType(String)} instead
+     * @param eventType
+     */
+    @Deprecated
+    @JsonIgnore
+    public void setEvent_type(String eventType) {
+        setEventType(eventType);
     }
 }
