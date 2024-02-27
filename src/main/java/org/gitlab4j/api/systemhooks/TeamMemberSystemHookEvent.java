6c06521abedebedad64a47b48960f570fba48dd7
Vimsucks <dev@vimsucks.com>
2024-02-28 05:26:37 +0800

    Fix wrongly named field in TeamMemberSystemHookEvent (#1092)

--- src/main/java/org/gitlab4j/api/systemhooks/TeamMemberSystemHookEvent.java
+++ src/main/java/org/gitlab4j/api/systemhooks/TeamMemberSystemHookEvent.java
@@ -14,7 +14,9 @@ public class TeamMemberSystemHookEvent extends AbstractSystemHookEvent {
     private Date createdAt;
     private Date updatedAt;
     private String eventName;
+    @Deprecated
     private String projectAccess;
+    private String accessLevel;
     private String projectName;
     private String projectPath;
     private Long projectId;
@@ -50,12 +52,28 @@ public class TeamMemberSystemHookEvent extends AbstractSystemHookEvent {
         this.eventName = eventName;
     }
 
+    /**
+     * No longer used. Probably replaced by {@link #getAccessLevel()}
+     */
+    @Deprecated
     public String getProjectAccess() {
         return projectAccess;
     }
 
+    /**
+     * No longer used. Probably replaced by {@link #setAccessLevel(String)}
+     */
+    @Deprecated
     public void setProjectAccess(String projectAccess) {
         this.projectAccess = projectAccess;
+    }
+
+    public String getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(String accessLevel) {
+        this.accessLevel = accessLevel;
     }
 
     public String getProjectName() {
