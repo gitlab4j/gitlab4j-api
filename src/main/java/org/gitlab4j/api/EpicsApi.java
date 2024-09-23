35d7add94c56ea8a41ae671170d15a882638db6a
Jeremie Bresson <jeremie.bresson@unblu.com>
2024-09-23 14:16:23 +0200

    Add support for "created_at" in Epic creation

--- src/main/java/org/gitlab4j/api/EpicsApi.java
+++ src/main/java/org/gitlab4j/api/EpicsApi.java
@@ -259,15 +259,38 @@ public class EpicsApi extends AbstractApi {
      * @param endDate the end date of the epic (optional)
      * @return an Epic instance containing info on the newly created epic
      * @throws GitLabApiException if any exception occurs
+     * @deprecated use {@link #createEpic(Object, String, String, String, Date, Date, Date)} instead
      */
+    @Deprecated
     public Epic createEpic(Object groupIdOrPath, String title, String labels, String description,
             Date startDate, Date endDate) throws GitLabApiException {
+        return createEpic(groupIdOrPath, title, labels, description, startDate, endDate, null);
+    }
+
+    /**
+     * Creates a new epic.
+     *
+     * <pre><code>GitLab Endpoint: POST /groups/:id/epics</code></pre>
+     *
+     * @param groupIdOrPath the group ID, path of the group, or a Group instance holding the group ID or path
+     * @param title the title of the epic (required)
+     * @param labels comma separated list of labels (optional)
+     * @param description the description of the epic (optional)
+     * @param startDate the start date of the epic (optional)
+     * @param endDate the end date of the epic (optional)
+     * @param createdAt the end date when the epic was created. Requires administrator or project/group owner privileges (optional)
+     * @return an Epic instance containing info on the newly created epic
+     * @throws GitLabApiException if any exception occurs
+     */
+    public Epic createEpic(Object groupIdOrPath, String title, String labels, String description,
+            Date startDate, Date endDate, Date createdAt) throws GitLabApiException {
         Form formData = new GitLabApiForm()
                 .withParam("title", title, true)
                 .withParam("labels", labels)
                 .withParam("description", description)
                 .withParam("start_date", startDate)
-                .withParam("end_date", endDate);
+                .withParam("end_date", endDate)
+                .withParam("created_at", createdAt);
         Response response = post(Response.Status.CREATED, formData.asMap(),
                 "groups", getGroupIdOrPath(groupIdOrPath), "epics");
         return (response.readEntity(Epic.class));
@@ -297,7 +320,8 @@ public class EpicsApi extends AbstractApi {
                 .withParam("labels", epic.getLabels())
                 .withParam("description", epic.getDescription())
                 .withParam("start_date", epic.getStartDate())
-                .withParam("end_date", epic.getEndDate());
+                .withParam("end_date", epic.getEndDate())
+                .withParam("created_at", epic.getCreatedAt());
         Response response = post(Response.Status.CREATED, formData.asMap(),
                 "groups", getGroupIdOrPath(groupIdOrPath), "epics");
         return (response.readEntity(Epic.class));
