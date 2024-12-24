a7cd937f4dfcc7b999a9d0801c74cf7bbed9c944
Jérémie Bresson <jeremie.bresson@unblu.com>
2024-12-24 14:26:32 +0100

    Remove classes to prevent "split packages" issues (#1216)

--- gitlab4j-api/src/main/java/org/gitlab4j/api/webhook/WebHookManager.java
+++
@@ -1,9 +0,0 @@
-package org.gitlab4j.api.webhook;
-
-import org.gitlab4j.api.HookManager;
-
-/**
- * @deprecated use {@link org.gitlab4j.api.WebHookManager} instead
- */
-@Deprecated
-public class WebHookManager extends org.gitlab4j.api.WebHookManager implements HookManager {}
