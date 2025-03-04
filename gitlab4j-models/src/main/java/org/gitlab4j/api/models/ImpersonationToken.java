1627d509400bcd4f9de092e9e8c30cceddc62da4
Jeremie Bresson <jeremie.bresson@unblu.com>
2025-03-04 18:28:55 +0100

    Support description in the group and personal access tokens

--- gitlab4j-models/src/main/java/org/gitlab4j/api/models/ImpersonationToken.java
+++ gitlab4j-models/src/main/java/org/gitlab4j/api/models/ImpersonationToken.java
@@ -49,6 +49,7 @@ public class ImpersonationToken implements Serializable {
     private Long userId;
     private Boolean revoked;
     private String name;
+    private String description;
     private Long id;
     private Date createdAt;
     private Date lastUsedAt;
@@ -103,6 +104,14 @@ public class ImpersonationToken implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Long getId() {
