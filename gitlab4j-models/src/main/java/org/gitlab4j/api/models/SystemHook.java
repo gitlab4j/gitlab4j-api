6e91cb36dc2f92ced9ca9efd56f5f05b2849625e
Jérémie Bresson <jeremie.bresson@unblu.com>
2025-03-07 14:45:21 +0100

    System hook improvements (#1234)

--- gitlab4j-models/src/main/java/org/gitlab4j/api/models/SystemHook.java
+++ gitlab4j-models/src/main/java/org/gitlab4j/api/models/SystemHook.java
@@ -2,6 +2,7 @@ package org.gitlab4j.api.models;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import org.gitlab4j.models.utils.JacksonJson;
 
@@ -9,6 +10,8 @@ public class SystemHook implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;
+    private String name;
+    private String description;
     private String url;
     private Date createdAt;
     private Boolean pushEvents;
@@ -16,6 +19,7 @@ public class SystemHook implements Serializable {
     private Boolean enableSslVerification;
     private Boolean repositoryUpdateEvents;
     private Boolean mergeRequestsEvents;
+    private List<SystemHook.UrlVariable> urlVariables;
 
     public Long getId() {
         return id;
@@ -23,6 +27,22 @@ public class SystemHook implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getUrl() {
@@ -81,48 +101,85 @@ public class SystemHook implements Serializable {
         return mergeRequestsEvents;
     }
 
+    public List<SystemHook.UrlVariable> getUrlVariables() {
+        return urlVariables;
+    }
+
+    public void setUrlVariables(List<SystemHook.UrlVariable> urlVariables) {
+        this.urlVariables = urlVariables;
+    }
+
     public SystemHook withId(Long id) {
         this.id = id;
-        return (this);
+        return this;
+    }
+
+    public SystemHook withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public SystemHook withDescription(String description) {
+        this.description = description;
+        return this;
     }
 
     public SystemHook withUrl(String url) {
         this.url = url;
-        return (this);
+        return this;
     }
 
     public SystemHook withCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
-        return (this);
+        return this;
     }
 
     public SystemHook withPushEvents(Boolean pushEvents) {
         this.pushEvents = pushEvents;
-        return (this);
+        return this;
     }
 
     public SystemHook withTagPushEvents(Boolean tagPushEvents) {
         this.tagPushEvents = tagPushEvents;
-        return (this);
+        return this;
     }
 
     public SystemHook withEnableSslVerification(Boolean enableSslVerification) {
         this.enableSslVerification = enableSslVerification;
-        return (this);
+        return this;
     }
 
     public SystemHook withRepositoryUpdateEvents(Boolean repositoryUpdateEvents) {
         this.repositoryUpdateEvents = repositoryUpdateEvents;
-        return (this);
+        return this;
     }
 
     public SystemHook withMergeRequestsEvents(Boolean mergeRequestsEvents) {
         this.mergeRequestsEvents = mergeRequestsEvents;
-        return (this);
+        return this;
     }
 
     @Override
     public String toString() {
         return (JacksonJson.toJsonString(this));
+    }
+
+    public static class UrlVariable implements Serializable {
+        private static final long serialVersionUID = 1L;
+
+        private String key;
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        @Override
+        public String toString() {
+            return (JacksonJson.toJsonString(this));
+        }
     }
 }
