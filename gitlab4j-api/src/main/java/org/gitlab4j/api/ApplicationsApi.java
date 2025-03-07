aac48f1764671d64dd6917d2baaaea41e5750e2d
Jeremie Bresson <jeremie.bresson@unblu.com>
2025-03-07 16:36:39 +0100

    Fix the way scopes are joined

--- gitlab4j-api/src/main/java/org/gitlab4j/api/ApplicationsApi.java
+++ gitlab4j-api/src/main/java/org/gitlab4j/api/ApplicationsApi.java
@@ -135,7 +135,7 @@ public class ApplicationsApi extends AbstractApi {
             throw new GitLabApiException("scopes cannot be null or empty");
         }
 
-        String scopesString = scopes.stream().map(ApplicationScope::toString).collect(Collectors.joining(","));
+        String scopesString = scopes.stream().map(ApplicationScope::toString).collect(Collectors.joining(" "));
         GitLabApiForm formData = new GitLabApiForm()
                 .withParam("name", name, true)
                 .withParam("redirect_uri", redirectUri, true)
