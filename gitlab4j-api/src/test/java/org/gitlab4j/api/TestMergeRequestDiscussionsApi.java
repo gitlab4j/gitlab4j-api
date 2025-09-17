27f511a76bcedca4f43f2e0f1054377b51c38508
Lucas Laurindo dos Santos <lucas.santos@free-now.com>
2025-09-17 18:40:56 +0200

    Add methods to retrieve a single discussion from a Merge Request

--- gitlab4j-api/src/test/java/org/gitlab4j/api/TestMergeRequestDiscussionsApi.java
+++ gitlab4j-api/src/test/java/org/gitlab4j/api/TestMergeRequestDiscussionsApi.java
@@ -1,18 +1,6 @@
 package org.gitlab4j.api;
 
-import static org.gitlab4j.api.JsonUtils.compareJson;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import jakarta.ws.rs.core.MultivaluedMap;
-
 import org.gitlab4j.api.models.Discussion;
 import org.gitlab4j.models.Constants;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +9,18 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.gitlab4j.api.JsonUtils.compareJson;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 public class TestMergeRequestDiscussionsApi implements Constants {
 
@@ -38,7 +38,7 @@ public class TestMergeRequestDiscussionsApi implements Constants {
     @BeforeEach
     public void setUp() throws Exception {
         openMocks(this);
-        response = new MockResponse(Discussion.class, null, "merge-request-discussions.json");
+        response = new MockResponse(Discussion.class, "merge-request-discussion.json", "merge-request-discussions.json");
         when(gitLabApi.getApiClient()).thenReturn(gitLabApiClient);
         when(gitLabApiClient.validateSecretToken(any())).thenReturn(true);
         when(gitLabApiClient.get(attributeCaptor.capture(), Mockito.any(Object[].class)))
@@ -72,5 +72,19 @@ public class TestMergeRequestDiscussionsApi implements Constants {
         assertNotNull(stream);
         List<Discussion> discussions = stream.collect(Collectors.toList());
         assertTrue(compareJson(discussions, "merge-request-discussions.json"));
+    }
+
+    @Test
+    public void testGetMergeRequestDiscussion() throws Exception {
+        Discussion discussion = new DiscussionsApi(gitLabApi).getMergeRequestDiscussion(1L, 1L, "1");
+        assertNotNull(discussion);
+        assertTrue(compareJson(discussion, "merge-request-discussion.json"));
+    }
+
+    @Test
+    public void testGetOptionalMergeRequestDiscussion() throws Exception {
+        Optional<Discussion> discussion = new DiscussionsApi(gitLabApi).getOptionalMergeRequestDiscussion(1L, 1L, "");
+        assertTrue(discussion.isPresent());
+        assertTrue(compareJson(discussion.get(), "merge-request-discussion.json"));
     }
 }
